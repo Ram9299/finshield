f45a82ea7a7e9a7de25bceb568e2c4434fd2d7cb
Konda Reddy <konda.v.reddy@globallogic.com>
2026-02-24 15:22:06 +0530

    bootstrap spring boot project with postgres + flyway

---
+++ finshield/src/main/java/com/finshield/FinshieldApplication.java
@@ -0,0 +1,11 @@
+package com.finshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinshieldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinshieldApplication.class, args);
+    }
+}
---
+++ finshield/src/test/java/com/finshield/FinshieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinshieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
