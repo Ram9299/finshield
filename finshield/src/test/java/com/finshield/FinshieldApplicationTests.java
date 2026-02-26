76491fb75247a3f565138b17d6e3e7318515efa5
Ram9299 <2100039084cse.r@gmail.com>
2026-02-26 10:58:00 +0530

    test: remove default context load tests in favor of testcontainers IT

--- finshield/src/test/java/com/finshield/FinshieldApplicationTests.java
+++ finshield/src/test/java/com/finshield/FinshieldApplicationTests.java
@@ -1,8 +1,10 @@
 package com.finshield;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Disabled because integration tests use Testcontainers for DB/Redis")
 @SpringBootTest
 class FinshieldApplicationTests {
 
